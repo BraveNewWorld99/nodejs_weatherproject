11594fa0d3836ae3ee01db4b63ad09fba97e1068
Adam Hott <adamhott@hotmail.com>
2021-03-17 06:23:12 -0500

    completed, deleted API key

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const express = require("express");
 const app = express();
 
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
-    const query = "Chicago";
-    const apiKey = "f4fbbacad8e802cdbf1beb8e1dff776e";
+    res.sendFile(__dirname + "/index.html");
+
+})
+
+app.post("/", function(req, res) {
+
+    const query = req.body.cityName;
+    const apiKey = "deleted_for_security_purposes";
     const units = "imperial";
 
 
@@ -25,27 +34,13 @@ app.get("/", function(req, res) {
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p>The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in Chicago is " + temp + " degrees Fahrenheit. </h1>");
+            res.write("<h1>The temperature in " + query  +  " is " + temp + " degrees Fahrenheit. </h1>");
             res.write("<img src=" + imageURL + ">");
             res.send();
         })
     })
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
     console.log("server is running on port 3000");
